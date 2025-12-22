0ce2fdc464a75cc6ce707c36b9d4c6cd7d73a4b0
Sonu Kumar <sonusaini48292@gmail.com>
2025-12-22 08:53:30 +0530

    Deploy portfolio build to docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sonu-portfolio/'
+  base: '/sonu-portfolio/',
+  build: {
+    outDir: 'docs'
+  }
 })
